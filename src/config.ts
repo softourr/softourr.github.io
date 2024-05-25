89e511ce8601baea13af0cdc5543b2fcccd1b555
rhkrhz <67236054+kwawak@users.noreply.github.com>
2024-05-25 12:27:52 +0900

    fix CNAME, config.ts, add profile

--- src/config.ts
+++ src/config.ts
@@ -3,70 +3,71 @@ import type {
   NavBarConfig,
   ProfileConfig,
   SiteConfig,
-} from './types/config'
-import { LinkPreset } from './types/config'
+} from "./types/config";
+import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja'
+  title: "hez",
+  subtitle: "Demo Site",
+  lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false, // Hide the theme color picker for visitors
   },
   banner: {
     enable: false,
-    src: 'assets/images/demo-banner.png',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [
+    // Leave this array empty to use the default favicon
     // {
     //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
-  ]
-}
+  ],
+};
 
 export const navBarConfig: NavBarConfig = {
   links: [
     LinkPreset.Home,
     LinkPreset.Archive,
-    LinkPreset.About,
-    {
-      name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
-    },
+    // LinkPreset.About,
+    // {
+    //   name: "GitHub",
+    //   url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+    //   external: true, // Show an external link icon and will open in a new tab
+    // },
   ],
-}
+};
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  avatar: "assets/images/profile.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  name: "hez",
+  bio: "한글테스트",
   links: [
+    // {
+    //   name: "Twitter",
+    //   icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+    //   // You will need to install the corresponding icon set if it's not already included
+    //   // `pnpm add @iconify-json/<icon-set-name>`
+    //   url: "https://twitter.com",
+    // },
+    // {
+    //   name: "Steam",
+    //   icon: "fa6-brands:steam",
+    //   url: "https://store.steampowered.com",
+    // },
     {
-      name: 'Twitter',
-      icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
-                                        // You will need to install the corresponding icon set if it's not already included
-                                        // `pnpm add @iconify-json/<icon-set-name>`
-      url: 'https://twitter.com',
-    },
-    {
-      name: 'Steam',
-      icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
-    },
-    {
-      name: 'GitHub',
-      icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      name: "GitHub",
+      icon: "fa6-brands:github",
+      url: "https://github.com/hhzzzk",
     },
   ],
-}
+};
 
 export const licenseConfig: LicenseConfig = {
   enable: true,
-  name: 'CC BY-NC-SA 4.0',
-  url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-}
+  name: "CC BY-NC-SA 4.0",
+  url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+};
