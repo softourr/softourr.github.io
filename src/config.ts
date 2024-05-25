22167325db45cebae5d930ef690093cbf88dde05
rhkrhz <67236054+kwawak@users.noreply.github.com>
2024-05-25 15:42:34 +0900

    post fix, NavBar add about

--- src/config.ts
+++ src/config.ts
@@ -32,10 +32,11 @@ export const navBarConfig: NavBarConfig = {
   links: [
     LinkPreset.Home,
     LinkPreset.Archive,
+    LinkPreset.About,
     // LinkPreset.About,
     // {
     //   name: "GitHub",
-    //   url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+    //   url: "https://github.com/saicaca/hhzzzk", // Internal links should not include the base path, as it is automatically added
     //   external: true, // Show an external link icon and will open in a new tab
     // },
   ],
---
+++ src/content/posts/helloworld.md
@@ -0,0 +1,15 @@
+---
+title: "Hello World!"
+published: 2024-05-25
+description: "무작정 기술 블로그 만들기"
+image: "./img/helloworld.gif"
+tags: [challenge]
+category: challenge
+draft: false
+---
+
+
+
+# to be continued...
+
+우하하 해냈다
