629081922db27277f3736edb20f4118e08d8661a
hyejeong <67236054+softourr@users.noreply.github.com>
2025-03-14 01:33:04 +0900

    Update: page width

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -15,5 +15,5 @@ export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND
 // The height the main panel overlaps the banner, unit: rem
 export const MAIN_PANEL_OVERLAPS_BANNER_HEIGHT = 3.5
 
-// Page width: rem
-export const PAGE_WIDTH = 75
+// Page width: rem 75 basic
+export const PAGE_WIDTH = 85
