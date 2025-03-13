c59d1a8f0b4b2e20c0f041b0f007840fb98d3f76
hyejeong <67236054+softourr@users.noreply.github.com>
2025-03-14 02:14:30 +0900

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,8 +8,8 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Akashic Records',
-  subtitle: 'Demo Site',
-  lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  subtitle: '🪽',
+  lang: 'ko',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 220,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
